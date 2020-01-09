67a1e54b9f1c850d02f044db2be82037f0b7ca62
JakubOlkowicz <olkuba@poczta.fm>
2020-01-09 15:58:20 +0100

    add code processOrder not working

--- src/js/script.js
+++ src/js/script.js
@@ -134,9 +134,40 @@
     }
     processOrder(){
       const thisProduct = this;
+      
+      /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
       const formData = utils.serializeFormToObject(thisProduct.form);
       console.log('formData', formData);
+      /* set variable price to equal thisProduct.data.price */
+      let price = thisProduct.data.price;
+      /* START LOOP: for each paramId in thisProduct.data.params */
+      for(let paramId in thisProduct.data.params){
+        /* save the element in thisProduct.data.params with key paramId as const param */
+        const param =  thisProduct.data.params[paramId];
+        /* START LOOP: for each optionId in param.options */
+        for(let optionId in param.options){
+          /* save the element in param.options with key optionId as const option */
+          const option = param.option[optionId];
+          /* START IF: if option is selected and option is not default */
+          if (option == formData && !option.default){
+            /* add price of option to variable price */
+            price = price += option;
+          /* END IF: if option is selected and option is not default */
+          }
+          /* START ELSE IF: if option is not selected and option is default */
+          else if (option != formData && !option.default){
+            /* deduct price of option from price */
+            price = price -= option;
+          }
+          /* END ELSE IF: if option is not selected and option is default */
+        }
+        /* END LOOP: for each optionId in param.options */
+      }
+      /* END LOOP: for each paramId in thisProduct.data.params */
+      thisProduct.priceElem = price += thisProduct.priceElem;
+      /* set the contents of thisProduct.priceElem to be the value of variable price */
     }
+      
   }
   const app = {
     initMenu: function(){
