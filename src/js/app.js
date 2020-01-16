bf7e240a203468f480854562fb54c7e13510b4bf
JakubOlkowicz <olkuba@poczta.fm>
2020-01-16 15:07:16 +0100

    add code initPages

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,60 @@ import Product from './componets/Product.js';
 import Cart from './componets/Cart.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/','');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListner('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        // get page id from href attribute
+        const id = clickedElement.getAtrribut('href').replace('#', '');
+
+        // run thisApp.activePage whit that id
+        thisApp.activePage(id);
+
+        // change URL hash
+
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activePage: function(pageId){
+    const thisApp = this;
+
+    // add class 'active' to matching pages, remove from non-matching
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    // add class 'active' to matching links, remove from non-matching
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAtrribut('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;
 
@@ -44,16 +98,16 @@ const app = {
   },
   init: function(){
     const thisApp = this;
-    console.log('*** App starting ***');
-    console.log('thisApp:', thisApp);
-    console.log('classNames:', classNames);
-    console.log('settings:', settings);
-    console.log('templates:', templates);
+    // console.log('*** App starting ***');
+    // console.log('thisApp:', thisApp);
+    // console.log('classNames:', classNames);
+    // console.log('settings:', settings);
+    // console.log('templates:', templates);
 
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
-  },
-    
+  },   
 };
 app.init();
 
